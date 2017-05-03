adaa44929edb89c66f28d18c769d553a2246d155
Sean Lomax <sean.lomax@objectpartners.com>
2017-05-03 17:45:38 -0500

    adding some valid attributes to the game model

--- app/models/game.js
+++ app/models/game.js
@@ -1,5 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr('string')
+  playerOneName: DS.attr('string'),
+  playerTwoName: DS.attr('string'),
+  playerOneWins: DS.attr('number'),
+  playerTwoWins: DS.attr('number'),
+  playerOneChoice: DS.attr('string'),
+  playerTwoChoice: DS.attr('string'),
+  timer: DS.attr('number'),
+  hostUser: DS.attr('number')
 });
