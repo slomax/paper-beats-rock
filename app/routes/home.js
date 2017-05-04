23f8d448f9390d5a95175ceb986241c7417352ef
Sean Lomax <sean.lomax@objectpartners.com>
2017-05-04 11:20:59 -0500

    should have included this in previous commit

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,14 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(params) {
-    const gameId = params['gameId'];
-    const joiningGame = gameId ? true : false;
 
-    return {
-      showNameDialog: joiningGame,
-      joiningGame: joiningGame,
-      gameId: gameId
-    }
-  }
 });
