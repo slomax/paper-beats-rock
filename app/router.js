87a50c7b0767d825b35d1e5698d7ae611a3e0ffa
Sean Lomax <sean.lomax@objectpartners.com>
2017-05-04 16:08:40 -0500

    this route is no longer possible

--- app/router.js
+++ app/router.js
@@ -8,7 +8,6 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about');
-  this.route('home', { path: '/join/:gameId'});
   this.route('home');
   this.route('game', { path: '/game/:gameId' });
 });
