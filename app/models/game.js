4b22af2c51026f8860e48db6854970b4e7ca81df
Sean Lomax <sean.lomax@objectpartners.com>
2017-05-05 13:24:27 -0500

    taking stuff out of the model that doesn’t need to be tracked in firebase

--- app/models/game.js
+++ app/models/game.js
@@ -3,25 +3,16 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   playerOneName: DS.attr('string'),
   playerTwoName: DS.attr('string'),
-  playerOneWins: DS.attr('number'),
-  playerTwoWins: DS.attr('number'),
   playerOneChoice: DS.attr('string'),
   playerTwoChoice: DS.attr('string'),
   timer: DS.attr('number'),
-  gameStarted: DS.attr('boolean'),
-  inputsDisabled: DS.attr('boolean'),
+  gameStarted: DS.attr('boolean')
 
   setPlayerOneName(playerOneName) {
     this.set('playerOneName', playerOneName);
   },
   setPlayerTwoName(playerTwoName) {
     this.set('playerTwoName', playerTwoName);
-  },
-  setPlayerOneWins(playerOneWins) {
-    this.set('playerOneWins', playerOneWins);
-  },
-  setPlayerTwoWins(playerTwoWins) {
-    this.set('playerTwoWins', playerTwoWins);
   },
   setPlayerOneChoice(playerOneChoice) {
     this.set('playerOneChoice', playerOneChoice);
@@ -40,12 +31,6 @@ export default DS.Model.extend({
   },
   getPlayerTwoName() {
     return this.get('playerTwoName');
-  },
-  getPlayerOneWins() {
-    return this.get('playerOneWins');
-  },
-  getPlayerTwoWins() {
-    return this.get('playerTwoWins');
   },
   getPlayerOneChoice() {
     return this.get('playerOneChoice');
